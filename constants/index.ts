60e21e1b077c0bd2e7ca0b5f7abc664f69b6550a
FATEH-GH <fatehghribi27@gmail.com>
2024-01-03 20:34:33 +0100

    prject description

--- constants/index.ts
+++ constants/index.ts
@@ -1,5 +1,14 @@
 export const projects = [
   {
+    mainProject: "/bg-project-3.png",
+    subProject1: "/project-3-1.png",
+    subProject2: "/project-3-2.png",
+    title: "GYMANIA",
+    link: "https://gymania.vercel.app/",
+    describtion:
+      "Welcome to GYMANIA, your go-to destination for a diverse and dynamic collection of gym exercises!  our website is designed to empower you with a wealth of workout options tailored to meet your goals.",
+  },
+  {
     mainProject: "/bg-project-1.png",
     subProject1: "/project-1-1.png",
     subProject2: "/project-1-2.png",
@@ -14,15 +23,6 @@ export const projects = [
     subProject2: "/project-2-2.png",
     title: "Hilink",
     link: "https://travel-sigma-fawn.vercel.app/",
-    describtion:
-      "Only with the hilink application you will no longer get lost, because we already support offline maps when there is no internet connection in the field, i helped designing the website.",
-  },
-  {
-    mainProject: "/bg-project-3.png",
-    subProject1: "/project-3-1.png",
-    subProject2: "/project-3-2.png",
-    title: "GYMANIA",
-    link: "https://gymania.vercel.app/",
     describtion:
       "Only with the hilink application you will no longer get lost, because we already support offline maps when there is no internet connection in the field, i helped designing the website.",
   },
