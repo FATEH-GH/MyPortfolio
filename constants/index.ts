05d0718ec8dca9a865fd10d7e1ca48a2f74370dd
FATEH-GH <fatehghribi27@gmail.com>
2024-02-11 10:02:27 +0100

    added a new project (weather)

--- constants/index.ts
+++ constants/index.ts
@@ -1,5 +1,14 @@
 export const projects = [
   {
+    mainProject: "/bg-project-6.png",
+    subProject1: "/project-6-1.png",
+    subProject2: "/project-6-2.png",
+    title: "Weather",
+    link: "https://weather-nu-nine.vercel.app/",
+    describtion:
+      "Weather is a sleek and user-friendly weather application designed to provide real-time weather updates at your fingertips. With a clean and intuitive interface, users can effortlessly access accurate weather information for any location around the globe..",
+  },
+  {
     mainProject: "/bg-project-3.png",
     subProject1: "/project-3-1.png",
     subProject2: "/project-3-2.png",
@@ -7,6 +16,16 @@ export const projects = [
     link: "https://gymania.vercel.app/",
     describtion:
       "Welcome to GYMANIA, your go-to destination for a diverse and dynamic collection of gym exercises!  our website is designed to empower you with a wealth of workout options tailored to meet your goals.",
+  },
+
+  {
+    mainProject: "/bg-project-5.png",
+    subProject1: "/project-5-1.png",
+    subProject2: "/project-5-2.png",
+    title: "Dropbox2.0",
+    link: "https://dropbox-zeta.vercel.app/",
+    describtion:
+      "Dropbox has proven to be extremely useful for social networks, so i tried to make a clone. Feel free to test it your self you can sign up to have the the full user experience (your data will be deleted as soon as you sign out) enjoy :P.",
   },
   {
     mainProject: "/bg-project-1.png",
@@ -17,15 +36,7 @@ export const projects = [
     describtion:
       "Discover stylish Adidas arrivals, quality confort, and innovation for your active life. A nodern adidas landing page that i chose to remake, which is responsive across all devices (tablets, laptops, mobile...) hope you like it :) .",
   },
-  {
-    mainProject: "/bg-project-5.png",
-    subProject1: "/project-5-1.png",
-    subProject2: "/project-5-2.png",
-    title: "Dropbox2.0",
-    link: "https://dropbox-zeta.vercel.app/",
-    describtion:
-      "Dropbox has proven to be extremely useful for social networks, so i tried to make a clone. Feel free to test it your self you can sign up to have the the full user experience (your data will be deleted as soon as you sign out) enjoy :P.",
-  },
+
   // {
   //   mainProject: "/bg-project-4.png",
   //   subProject1: "/project-4-1.png",
