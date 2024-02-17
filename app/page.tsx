16cf76a9d1fdd1eb384810deea753b5814466c3a
FATEH-GH <fatehghribi27@gmail.com>
2024-02-17 20:56:20 +0100

    re moved the 3D effect

--- app/page.tsx
+++ app/page.tsx
@@ -2,13 +2,11 @@ import Hero from "@/components/Hero";
 import About from "@/components/About";
 import Projects from "@/components/Projects";
 import EmailSection from "@/components/EmailSection";
-import DStar from "@/components/DStar";
 
 export default function Home() {
   return (
     <main>
       <Hero />
-      <DStar />
       <About />
       <Projects />
       <EmailSection />
