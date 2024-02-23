bc7d816a57292591a3e2c17e2fff3e3828bd0e6d
FATEH-GH <fatehghribi27@gmail.com>
2024-02-23 15:10:49 +0100

    fixed the useeffect

--- components/ui/TracingBeam.tsx
+++ components/ui/TracingBeam.tsx
@@ -39,7 +39,7 @@ export const TracingBeam = ({
     return scrollYProgressVelocity.onChange((latestVelocity) => {
       setVelocity(latestVelocity);
     });
-  }, []);
+  }, [scrollYProgressVelocity]);
 
   const y1 = useSpring(
     useTransform(scrollYProgress, [0, 0.85], [50, svgHeight]),
