1f10cd32f8173ee5422de6dfe67d090afa217314
FATEH-GH <fatehghribi27@gmail.com>
2024-02-23 15:05:38 +0100

    added side animation for scrolling

--- app/page.tsx
+++ app/page.tsx
@@ -3,15 +3,20 @@ import About from "@/components/About";
 import Projects from "@/components/Projects";
 import EmailSection from "@/components/EmailSection";
 import Skills from "@/components/Skills";
+import { TracingBeam } from "@/components/ui/TracingBeam";
 
 export default function Home() {
   return (
     <main>
-      <Hero />
-      <About />
-      <Skills />
-      <Projects />
-      <EmailSection />
+      <TracingBeam>
+        <div className=" mx-auto antialiased pt-4 relatived">
+          <Hero />
+          <About />
+          <Skills />
+          <Projects />
+          <EmailSection />
+        </div>
+      </TracingBeam>
     </main>
   );
 }
