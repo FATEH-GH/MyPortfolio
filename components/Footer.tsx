84c4a90d68481f0328485e2738e69ed79d510768
FATEH-GH <fatehghribi27@gmail.com>
2024-02-24 19:36:25 +0100

    removed name from footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -4,17 +4,14 @@ import { Icons } from "./Icons";
 const Footer = () => {
   return (
     <footer className="bg-gradient-to-b from-slate-200 to-slate-400 dark:from-[#28282d] dark:to-[#101012] p-4 rounded-t-xl ">
-      <div className="flex flex-col lg:flex-row items-center lg:items-start lg:justify-evenly">
-        <div className="flex items-center gap-4">
+      <div className="flex flex-col lg:flex-row items-center lg:items-start lg:justify-between border-2">
+        <div className="border-2 flex items-center  justify-center">
           <Link
             href="/"
             className="font-montserrat font-extrabold lg:p-4 p-2 lg:max-w-[60px] max-w-[60px] text-lg sm:text-lg lg:text-xl  rounded-full bg-[#f0be6d] text-black dark:text-slate-200 "
           >
             FG
           </Link>
-          <p className="text-[16px] sm:text-lg xl:text-xl font-bold text-white transition duration-300">
-            ABDENNOUR FATEH GHRIBI
-          </p>
         </div>
         <div className="flex flex-col items-center justify-center gap-4 mb-4">
           <h3 className="font-montserrat font-extrabold text-xl  sm:text-lg lg:text-xl xl:text-3xl  text-white">
