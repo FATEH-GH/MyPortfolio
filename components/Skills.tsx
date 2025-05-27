fad61cfeab5197057b48571f7cb4bfcfced99cba
FATEH-GH <fatehghribi27@gmail.com>
2025-05-27 22:43:32 +0100

    added a new feature in the skills section

--- components/Skills.tsx
+++ components/Skills.tsx
@@ -1,91 +1,93 @@
 "use client";
 
-import { DivMotion } from "./FramerMotion";
 import { FaHtml5, FaCss3Alt, FaGitAlt, FaReact, FaNode } from "react-icons/fa";
 import { TbBrandNextjs } from "react-icons/tb";
 import { IoLogoJavascript } from "react-icons/io5";
-import { SiTailwindcss, SiFramer } from "react-icons/si";
+import { SiTailwindcss } from "react-icons/si";
 import { LiaDocker } from "react-icons/lia";
 import { BsFiletypeSql } from "react-icons/bs";
-import { useRef } from "react";
-import { useInView } from "framer-motion";
+import { BiLogoMongodb } from "react-icons/bi";
+import { HoverEffect } from "./ui/Card-hover-effect";
 
 const Skills = () => {
-  const ref = useRef(null);
-  const isInView = useInView(ref, { once: true });
   return (
-    <section id="Skills" className="font-montserrat text-white  ">
-      <DivMotion
-        className="flex flex-col items-center justify-center mx-8"
-        initial={{ opacity: 0, y: 100 }}
-        transition={{ duration: 0.7, ease: "easeIn" }}
-        animate={isInView ? { opacity: 1, y: 0 } : {}}
-      >
+    <section id="Skills" className="font-montserrat text-white ">
+      <div className="flex flex-col items-center justify-center mx-8">
         <div className=" w-full">
           <h3 className=" flex text-black dark:text-[#ADB7BE] transition duration-300 justify-center text-4xl sm:text-5xl lg:text-6xl font-extrabold mb-4">
             Skills
           </h3>
           <div className="mt-1 h-1 bg-black dark:bg-[#ADB7BE]  transition duration-300  max-w-[100px] sm:max-w-[150px] flex justify-center m-auto mb-6 sm:mb-12" />
         </div>
-        <div
-          className="flex flex-col gap-6 mx-auto p-4 sm:p-20 rounded-xl font-bold"
-          ref={ref}
-        >
-          <div className="flex flex-wrap gap-4 itmes-center justify-center">
-            <div className=" bg-[#3f3f40]  rounded-lg w-[100px] flex flex-col gap-2 items-center justify-center p-4 sm:w-[125px]">
-              <FaHtml5 size="50px" color="#f16b31" />
-              <span>html</span>
-            </div>
-            <div className="bg-[#3f3f40] rounded-lg w-[100px] flex flex-col items-center justify-center p-4 sm:w-[125px]">
-              <FaCss3Alt size="50" color="#2d52e4" />
-              <span>Css</span>
-            </div>
-            <div className="bg-[#3f3f40] rounded-lg w-[100px] flex flex-col items-center justify-center p-4 sm:w-[125px]">
-              <IoLogoJavascript size="50" color="yellow" />
-              <span>javascript</span>
-            </div>
-            <div className="bg-[#3f3f40] rounded-lg w-[100px] flex flex-col items-center justify-center p-4 sm:w-[125px]  ">
-              <SiTailwindcss size="50" color="#0ea4e8" />
-              <span>Tailwindcss</span>
-            </div>
-          </div>
-          <div className="flex flex-wrap gap-4 itmes-center justify-center">
-            <div className="bg-[#3f3f40] rounded-lg w-[100px] flex flex-col items-center justify-center p-4 sm:w-[125px]">
-              <LiaDocker size="50" color="#2bbbeb" />
-              <span>Docker</span>
-            </div>
-            <div className="bg-[#3f3f40] rounded-lg w-[100px] flex flex-col items-center justify-center p-4 sm:w-[125px]">
-              <FaGitAlt size="50" color="#f05438" />
-              <span>Git</span>
-            </div>
-
-            <div className="bg-[#3f3f40] rounded-lg w-[100px] flex flex-col items-center justify-center p-4 sm:w-[125px]">
-              <BsFiletypeSql size="50" color="orange" />
-              <span>SQL</span>
-            </div>
-            <div className="bg-[#3f3f40] rounded-lg w-[100px] flex flex-col items-center justify-center p-4 sm:w-[125px]">
-              <SiFramer size="50" color="white" />
-              <span>Framer</span>
-            </div>
-          </div>
-          <div className="flex flex-wrap gap-4 itmes-center justify-center">
-            <div className="bg-[#3f3f40] rounded-lg w-[100px] flex flex-col items-center justify-center p-4 sm:w-[125px]">
-              <FaReact size="50" color="#2bbbeb" />
-              <span>ReactJs</span>
-            </div>
-            <div className="bg-[#3f3f40] rounded-lg w-[100px] flex flex-col items-center justify-center p-4 sm:w-[125px]">
-              <FaNode size="75" color="green" />
-              <span>Node.js</span>
-            </div>
-            <div className="bg-[#3f3f40] rounded-lg w-[100px] flex flex-col items-center justify-center p-4 sm:w-[125px]">
-              <TbBrandNextjs size="50" color="white" />
-              <span>NextJs</span>
-            </div>
-          </div>
-        </div>
-      </DivMotion>
+        <HoverEffect items={IconSkills} />
+      </div>
     </section>
   );
 };
+
+const IconSkills = [
+  {
+    icon: FaHtml5,
+    label: "HTML",
+    color: "#f16b31",
+  },
+  {
+    icon: FaCss3Alt,
+    label: "CSS",
+    color: "#2d52e4",
+  },
+  {
+    icon: IoLogoJavascript,
+    label: "Javascipt",
+    color: "yellow",
+  },
+
+  {
+    icon: SiTailwindcss,
+    label: "Tailwindcss",
+    color: "#0ea4e8",
+  },
+
+  {
+    icon: LiaDocker,
+    label: "Docker",
+    color: "#2bbbeb",
+  },
+
+  {
+    icon: FaGitAlt,
+    label: "Git",
+    color: "#f05438",
+  },
+
+  {
+    icon: BsFiletypeSql,
+    label: "SQL",
+    color: "orange",
+  },
+
+  {
+    icon: BiLogoMongodb,
+    label: "MongoDB",
+    color: "white",
+  },
+
+  {
+    icon: FaReact,
+    label: "ReactJs",
+    color: "#2bbbeb",
+  },
+
+  {
+    icon: FaNode,
+    color: "green",
+    label: "Node.js",
+  },
+  {
+    icon: TbBrandNextjs,
+    color: "white",
+    label: "NEXT.JS",
+  },
+];
 
 export default Skills;
---
+++ components/ui/Card-hover-effect.tsx
@@ -0,0 +1,58 @@
+"use client";
+
+import { cn } from "@/lib/utils";
+import { AnimatePresence, motion } from "motion/react";
+
+import { useState } from "react";
+import { IconType } from "react-icons";
+
+console.log("hello there ");
+
+export const HoverEffect = ({
+  items,
+  className,
+}: {
+  items: {
+    icon: IconType;
+    label: string;
+    color: string;
+  }[];
+  className?: string;
+}) => {
+  let [hoveredIndex, setHoveredIndex] = useState<number | null>(null);
+
+  return (
+    <div className={cn("flex flex-wrap justify-center max-w-2xl  ", className)}>
+      {items.map((item, idx) => (
+        <div
+          key={item?.label}
+          className="relative group   p-2"
+          onMouseEnter={() => setHoveredIndex(idx)}
+          onMouseLeave={() => setHoveredIndex(null)}
+        >
+          <AnimatePresence>
+            {hoveredIndex === idx && (
+              <motion.span
+                className="absolute inset-0 h-full w-full bg-[#efc787] block  rounded-3xl -z-10"
+                layoutId="hoverBackground"
+                initial={{ opacity: 0 }}
+                animate={{
+                  opacity: 1,
+                  transition: { duration: 0.15 },
+                }}
+                exit={{
+                  opacity: 0,
+                  transition: { duration: 0.15, delay: 0.2 },
+                }}
+              />
+            )}
+          </AnimatePresence>
+          <div className=" bg-[#3f3f40]  rounded-lg w-[100px] flex flex-col gap-2 items-center justify-center p-4 sm:w-[125px] ">
+            <item.icon size="50px" color={item.color} />
+            <span>{item.label}</span>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
+};
