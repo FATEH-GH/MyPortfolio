5e086ef4991d1689113dbb2e6f9e2dcadc2f978e
FATEH-GH <fatehghribi27@gmail.com>
2025-08-04 12:01:26 +0100

    restyled the projects section

--- components/ProjectCard.tsx
+++ components/ProjectCard.tsx
@@ -1,68 +1,194 @@
+"use client";
+
 import { projects } from "@/constants";
 import Image from "next/image";
 import Link from "next/link";
-import { ImageMotion } from "./FramerMotion";
+import { useEffect, useId, useRef, useState } from "react";
+import { AnimatePresence, motion } from "motion/react";
 
-const ProjectCard = () => {
+const Projectproject = () => {
+  const [active, setActive] = useState<
+    (typeof projects)[number] | boolean | null
+  >(null);
+  const ref = useRef<HTMLDivElement>(null);
+  const id = useId();
+
+  useEffect(() => {
+    function onKeyDown(event: KeyboardEvent) {
+      if (event.key === "Escape") {
+        setActive(false);
+      }
+    }
+
+    if (active && typeof active === "object") {
+      document.body.style.overflow = "hidden";
+    } else {
+      document.body.style.overflow = "auto";
+    }
+
+    window.addEventListener("keydown", onKeyDown);
+    return () => window.removeEventListener("keydown", onKeyDown);
+  }, [active]);
+
   return (
-    <div className=" grid grid-cols-1 md:grid-cols-2 grid-flow-row items-center  gap-10 sm:gap-20 mx-auto my-20 ">
-      {projects.map((project) => (
-        <div className="flex flex-col items-center gap-10" key={project.title}>
-          <div className="flex relative rounded-3xl hover:border-4  overflow-hidden hover:scale-105 duration-200 hover:border-customColor ">
-            <Image
-              src={project.mainProject}
-              alt="project image"
-              height={600}
-              width={600}
-            />
-
-            <ImageMotion
-              src={project.subProject1}
-              alt="project image"
-              height={100}
-              width={100}
-              className="absolute right-0 top-10 shadow-2xl rounded-2xl hover:rotate-12"
-              initial={{ rotate: 0, x: 100, y: 0 }}
-              transition={{ duration: 1, ease: "easeOut" }}
-              whileInView={{ rotate: -30, x: 0, y: 0 }}
-            />
-
-            <ImageMotion
-              src={project.subProject2}
-              alt="project image"
-              height={100}
-              width={100}
-              className="absolute left-0 top-10 shadow-2xl rounded-2xl"
-              initial={{ rotate: 0, x: -100, y: 0 }}
-              transition={{ duration: 1, ease: "easeOut" }}
-              whileInView={{ rotate: 30, x: 0, y: 0 }}
-            />
-          </div>
-          <div className="max-w-xl mx-4  text-black dark:text-[#ADB7BE]  transition duration-300 ">
-            <h3 className="font-semibold text-xl dark:text-white ">
-              {" "}
-              {project.title}{" "}
-            </h3>
-            <p className="my-3 font-semibold ">{project.describtion}</p>
-            <Link
-              href={project.link}
-              className="bg-customColor hover:bg-customColorHover flex items-center justify-center p-2 sm:p-4 group gap-4 w-40 rounded-full mx-auto group"
-              target="_blank"
+    <>
+      <AnimatePresence>
+        {active && typeof active === "object" && (
+          <motion.div
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            exit={{ opacity: 0 }}
+            className="fixed inset-0 bg-black/20 h-full w-full z-10 p-4"
+          />
+        )}
+      </AnimatePresence>
+      <AnimatePresence>
+        {active && typeof active === "object" ? (
+          <div className="fixed inset-0  grid place-items-center z-[100]">
+            <motion.div
+              layoutId={`project-${active.title}-${id}`}
+              ref={ref}
+              className=" relative w-full max-w-[800px]  h-screen md:h-fit md:max-h-[90%]  flex flex-col bg-white dark:bg-neutral-900 sm:rounded-3xl overflow-hidden p-4 "
             >
-              <p className="font-semibold text-white">Preview</p>
-              <Image
-                src="/RightIcon.svg"
-                alt="right icon"
-                height={24}
-                width={24}
-                className="group-hover:translate-x-3 duration-300 "
-              />
-            </Link>
+              <motion.button
+                key={`button-${active.title}-${id}`}
+                layout
+                initial={{
+                  opacity: 0,
+                }}
+                animate={{
+                  opacity: 1,
+                }}
+                exit={{
+                  opacity: 0,
+                  transition: {
+                    duration: 0.05,
+                  },
+                }}
+                className=" absolute top-2 right-2 flex-1 items-center justify-center rounded-full h-6 w-6 md:h-10 md:w-10 hover:scale-105 cursor-pointer text-customColor"
+                onClick={() => setActive(null)}
+              >
+                <span className="h-6 w-6 md:h-10 md:w-10 font-bold">X</span>
+              </motion.button>
+              <motion.div
+                layoutId={`image-${active.title}-${id}`}
+                className="rounded-2xl"
+              >
+                <Image
+                  width={600}
+                  height={600}
+                  src={active.mainProject}
+                  alt={active.title}
+                  className="w-full h-50 lg:h-80 object-contain object-center"
+                />
+              </motion.div>
+
+              <div>
+                <div className="flex justify-between items-start p-4">
+                  <div className="">
+                    <motion.h3
+                      layoutId={`title-${active.title}-${id}`}
+                      className="font-semibold text-xl text-neutral-700 dark:text-neutral-200"
+                    >
+                      {active.title}
+                    </motion.h3>
+                  </div>
+
+                  <Link
+                    href={active.link}
+                    target="_blank"
+                    className="px-4 py-3 text-sm rounded-full font-bold bg-customColor hover:bg-customColorHover text-white"
+                  >
+                    View
+                  </Link>
+                </div>
+                <div className="pt-4 relative px-4">
+                  <motion.div
+                    layout
+                    initial={{ opacity: 0 }}
+                    animate={{ opacity: 1 }}
+                    exit={{ opacity: 0 }}
+                    className="text-black dark:text-[#ADB7BE] text-sm md:text-sm lg:text-base h-40 md:h-fit pb-10 flex flex-col items-center gap-4 overflow-auto "
+                  >
+                    <p>{active.description}</p>
+                  </motion.div>
+                </div>
+              </div>
+            </motion.div>
           </div>
-        </div>
-      ))}
-    </div>
+        ) : null}
+      </AnimatePresence>
+      <ul className="max-w-4xl mx-auto w-full gap-4">
+        {projects.map((project, index) => (
+          <motion.div
+            layoutId={`project-${project.title}-${id}`}
+            key={`project-${project.title}-${id}`}
+            onClick={() => setActive(project)}
+            className="p-4 flex flex-col md:flex-row justify-between items-center hover:bg-neutral-50 dark:hover:bg-neutral-800 rounded-xl cursor-pointer"
+          >
+            <div className="flex gap-4 flex-col md:flex-row ">
+              <motion.div layoutId={`image-${project.title}-${id}`}>
+                <Image
+                  width={200}
+                  height={200}
+                  src={project.mainProject}
+                  alt={project.title}
+                  className="rounded-lg object-contain object-top"
+                />
+              </motion.div>
+              <div className="flex items-center justify-center">
+                <motion.h3
+                  layoutId={`title-${project.title}-${id}`}
+                  className="font-semibold text-xl dark:text-white text-neutral-800 text-center md:text-left"
+                >
+                  {project.title}
+                </motion.h3>
+              </div>
+            </div>
+            <motion.button
+              layoutId={`button-${project.title}-${id}`}
+              className="px-4 py-2  rounded-full font-bold bg-customColor   hover:bg-customColorHover  mt-4 md:mt-0"
+            >
+              View
+            </motion.button>
+          </motion.div>
+        ))}
+      </ul>
+    </>
   );
 };
 
-export default ProjectCard;
+export default Projectproject;
+
+export const CloseIcon = () => {
+  return (
+    <motion.svg
+      initial={{
+        opacity: 0,
+      }}
+      animate={{
+        opacity: 1,
+      }}
+      exit={{
+        opacity: 0,
+        transition: {
+          duration: 0.05,
+        },
+      }}
+      xmlns="http://www.w3.org/2000/svg"
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="orange"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      className="h-6 w-6 md:h-8 md:w-8 hover:scale-105 text-black"
+    >
+      <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+      <path d="M18 6l-12 12" />
+      <path d="M6 6l12 12" />
+    </motion.svg>
+  );
+};
--- constants/index.ts
+++ constants/index.ts
@@ -13,7 +13,7 @@ export const projects = [
     subProject2: "/project-3-2.png",
     title: "GYMANIA",
     link: "https://gymania.vercel.app/",
-    describtion:
+    description:
       "Welcome to GYMANIA, your go-to destination for a diverse and dynamic collection of gym exercises!  our website is designed to empower you with a wealth of workout options tailored to meet your goals.",
   },
   {
@@ -22,7 +22,7 @@ export const projects = [
     subProject2: "/project-1-2.png",
     title: "Adidas",
     link: "https://adidas-gamma.vercel.app/",
-    describtion:
+    description:
       "Discover stylish Adidas arrivals, quality confort, and innovation for your active life. A nodern adidas landing page that i chose to remake, which is responsive across all devices (tablets, laptops, mobile...) hope you like it :) .",
   },
   {
@@ -31,7 +31,7 @@ export const projects = [
     subProject2: "/project-6-2.png",
     title: "Weather",
     link: "https://weather-nu-nine.vercel.app/",
-    describtion:
+    description:
       "Weather is a sleek and user-friendly weather application designed to provide real-time weather updates at your fingertips. With a clean and intuitive interface, users can effortlessly access accurate weather information for any location around the globe..",
   },
   {
@@ -40,7 +40,7 @@ export const projects = [
     subProject2: "/project-5-2.png",
     title: "Dropbox2.0",
     link: "https://dropbox-zeta.vercel.app/",
-    describtion:
+    description:
       "Dropbox has proven to be extremely useful for social networks, so i tried to make a clone. Feel free to test it your self you can sign up to have the the full user experience (your data will be deleted as soon as you sign out) enjoy :P.",
   },
 
@@ -50,7 +50,7 @@ export const projects = [
     subProject2: "/project-2-2.png",
     title: "Hilink",
     link: "https://travel-sigma-fawn.vercel.app/",
-    describtion:
+    description:
       "Only with the hilink application you will no longer get lost, because we already support offline maps when there is no internet connection in the field, i helped designing the website.",
   },
 ];
